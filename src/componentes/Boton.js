9c562300cf26c2069dc69c3f3da7be52283d9774
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-07 17:57:48 +0100

    Update

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Boton(props) {
-    return(
+
+  const esOperador = valor => {
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
+  };
+
+    return (
      <div 
      className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}>
       {props.children}
